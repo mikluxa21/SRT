7b0f967cf07d081ef73af9bf4f5e01bac2e9140b
mikluxa21 <teterevkolya21@gmail.com>
2024-02-01 13:55:25 +0300

    add seconds lab

--- 2/TR2_Teterev_9363.c
+++ 2/TR2_Teterev_9363.c
@@ -15,36 +15,77 @@ int state = THIS_FLOOR;
 void move(int newState)
 {
 	usleep(1000000);
+	state = newState;
 }
 
 
-void liftControl(int signo)
+void liftControl(int signo, siginfo_t* info, void* context)
 {
-	std::cout <<signo << std::endl;
+	int val = info->si_value.sival_int;
 	switch(state){
 		case (THIS_FLOOR):
-			std::cout << THIS_FLOOR<< std::endl;
+			if(val = UP)
+				move(TOP);
+			if(val == DOWN)
+				move(BOTTOM);
 			break;
 		case (TOP):
-			std::cout << TOP << std::endl;
+			if(val = UP)
+				std::cout << "Error: state = TOP" << std::endl;
+			if(val == DOWN)
+				move(THIS_FLOOR);
 			break;
 		case (BOTTOM):
-			std::cout << BOTTOM << std::endl;
+			if(val = UP)
+				move(THIS_FLOOR);
+			if(val == DOWN)
+				std::cout << "Error: state = BOTTOM" << std::endl;
 			break;
 	}
-} 
+}
+
+void* push_buttom(void* args)
+{
+	char ch;
+	struct sigaction sa;
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = liftControl;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+	union sigval val;
+	while(ch != 'q')
+	{
+		std::cout << "Old state = " << state << std::endl;
+		std::cin >> ch;
+		switch(ch)
+		{
+			case 'u':
+				val.sival_int = UP;
+				sigqueue(getpid(), SIGUSR1, val);
+				break;
+			case 'd':
+				val.sival_int = DOWN;
+				sigqueue(getpid(), SIGUSR1, val);
+				break;
+			case 'q':
+				break;
+			default:
+				std::cout << "u - up\nd - dowm\nq - end\n";
+				break;
+		}
+		std::cout << "State after command = " << state << std::endl << std::endl;
+	}
+	return EXIT_SUCCESS;
+	
+}
 
 int main()
 {
-	sigset_t set;
-	sigemptyset(&set);
-	sigaddset(&set, SIGINT);
-	struct sigaction act;
-	act.sa_handler = &liftControl;
-	act.sa_flags = 0;
-	act.sa_mask = set;
-	sigaction(SIGINT, &act, NULL);
-	raise(BOTTOM);
+	std::cout << "Start" <<  std::endl;
+	pthread_t t;
+	pthread_create(&t, NULL, &push_buttom, NULL);
+	pthread_join(t, NULL);
+	std::cout << "Finish" << std::endl;
 	return 0;
 }
 
